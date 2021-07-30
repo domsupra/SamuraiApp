671f702ae056e4e1c9df392b5e4cf6752d99d15d
Dom <dship13@wgu.edu>
2021-07-30 12:49:20 -0600

    Add project files.

---
+++ SamuariApp.Data/SamuariContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuariApp.Data {
+   public class SamuariContext :DbContext{
+      public DbSet<Samurai> Samuaris { get; set; }
+      public DbSet<Quote> Quotes { get; set; }
+
+      protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSQLLocalDB; Initial Catalog=SamuraiAppData" );
+      }
+   }
+}
---
+++ SamuraiApp.Ui/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SamuraiApp.Ui {
+   class Program {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
